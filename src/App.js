2149b1af048af12b0d5c0dc5063e63694363d8ca
Raffaele Pansini <r_pansini@hotmail.com>
2022-12-26 15:34:56 +0100

    Added a git comment

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { getUser, getRepos } from './services/users'
 import { useParams } from 'react-router-dom'
 import Modal from './modal';
 
+// Added a comment
 function App() {
   const params = useParams()
   let username = params.user
